2a812eeef36d484b84871cc1bf372755b20dfecd
Ege Inanc <ege.inanc1991@gmail.com>
2023-10-15 16:34:34 +0200

    wip: camera preview

--- lib/camera_screen.dart
+++ lib/camera_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +13,47 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+  int selectedCamera = 0;
+  List<File> capturedImgs = [];
+
+  @override
+  void initState() {
+    initializeCamera(selectedCamera);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  initializeCamera(int cameraIndex) async {
+    _controller = CameraController(
+      widget.cameras[cameraIndex],
+      ResolutionPreset.max,
+    );
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: FutureBuilder<void>(
+          future: _initializeControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return CameraPreview(
+                _controller,
+                child: const Text("Hello world"),
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          }),
+    );
   }
 }
